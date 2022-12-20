54186e989394847fcf352f3f79ef77282f17f771
Jimmy <jimmydeveloperpasto@gmail.com>
2022-12-20 18:35:02 -0500

    aerva

--- public/worker.js
+++ public/worker.js
@@ -5,7 +5,7 @@ self.addEventListener('push', e=>{
   console.log(data);
   self.registration.showNotification(data.title, {
     body: data.message,
-    icon:'https://i.pinimg.com/236x/ce/59/83/ce59837dd46efcaa5549a75bf2b1e443.jpg',
+    icon:'https://cdn-icons-png.flaticon.com/512/4381/4381583.png',
     badge:'https://cdn-icons-png.flaticon.com/512/4381/4381609.png',
     click_action: 'https://www.flaticon.es/icono-gratis/reciclar_4381609'
   })
