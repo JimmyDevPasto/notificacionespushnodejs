5b774c65e865dbffe279e8c3903c776a0537164d
Jimmy <jimmydeveloperpasto@gmail.com>
2022-12-20 17:59:54 -0500

    aerva

--- public/worker.js
+++ public/worker.js
@@ -6,6 +6,6 @@ self.addEventListener('push', e=>{
   self.registration.showNotification(data.title, {
     body: data.message,
     icon:'https://i.pinimg.com/236x/ce/59/83/ce59837dd46efcaa5549a75bf2b1e443.jpg',
-    badge:'https://w1.pngwing.com/pngs/667/708/png-transparent-plastic-bag-waste-recycling-electronic-waste-recycling-symbol-recycling-bin-computer-recycling-waste-management.png'
+    badge:'reciclaje.png'
   })
 })
