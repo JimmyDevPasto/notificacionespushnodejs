774bac1b70e215cf85e5cde5c77f1ee2053b12ac
Jimmy <jimmydeveloperpasto@gmail.com>
2022-12-20 20:17:29 -0500

    a2231a

--- public/worker.js
+++ public/worker.js
@@ -3,18 +3,16 @@ const fecha = new Date();
 
 self.addEventListener('push', e=>{
   const data = e.data.json()
-  console.log(data);
-  self.registration.showNotification(data.title, {
+
+  const title= data.title;
+
+  const options= {
     body: data.message,
     icon:'https://scontent.fclo1-4.fna.fbcdn.net/v/t1.6435-9/119594937_107541191100279_7961959099134270346_n.png?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHqX4zZp3Wp_MRVb2bKcRDvmOFbjl6wS6qY4VuOXrBLqquXNMw5wZjYwQIN5-56n3gC393dWtko1tFlNQ8-bLm6&_nc_ohc=pY6KZPJiOPIAX8VWEKk&_nc_ht=scontent.fclo1-4.fna&oh=00_AfCUIGqOEdUb_TTk-Ojz-cFG7_yb_D5ZaqZoWAl7BK_nEQ&oe=63C9B02B',
     badge:'https://cdn-icons-png.flaticon.com/512/4381/4381609.png',
-    click_action: data.click_action,
-        requireInteraction: data.requireInteraction,
-        timestamp:data.timestamp,
-    
-  })
+  }
 
-  
+  self.registration.showNotification(title, options);  
 })
 
 self.addEventListener('notificationclick', event => {
